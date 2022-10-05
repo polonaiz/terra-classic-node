8beaf7f242d6e923b6b1de2a548de5319018c6f0
polonaiz <polonaiz@gmail.com>
2022-10-05 14:02:30 +0900

    update

--- scratch.sh
+++ scratch.sh
@@ -1,31 +1,4 @@
-# terrad from source
-cd ~
-sudo apt update 
-sudo apt install -y golang
-git clone https://github.com/terra-money/classic-core.git
-cd classic-core
-make install
-~/go/bin/terrad version
-sudo cp ~/go/bin/terrad /usr/local/bin/
-which terrad
-rm -rf ~/classic-core
 
-# # terrad from release
-# wget https://github.com/terra-money/classic-core/releases/download/v0.5.20/terra_0.5.20_Linux_x86_64.tar.gz
-# tar zxvf terra_0.5.20_Linux_x86_64.tar.gz
-# sudo mv ./terrad /usr/local/bin
-# rm ./terra_0.5.20_Linux_x86_64.tar.gz
-
-# init node
-rm -rf ~/.terra/
-terrad init private-node
-
-wget -O ~/.terra/config/genesis.json https://columbus-genesis.s3.ap-northeast-1.amazonaws.com/columbus-5-genesis.json
-# wget -O ~/.terra/config/addrbook.json https://dl2.quicksync.io/json/addrbook.terra.json # fail
-cp /workspaces/terra-classic-full-node/addrbook.json ~/.terra/config/addrbook.json
-
-# start node
-terrad start --x-crisis-skip-assert-invariants
 
 #
 gcloud auth list
@@ -35,6 +8,16 @@ gcloud container clusters get-credentials
 kubectl get storageclasses
 
 time tar --zstd -cf /tmp/.terra.config.tar.zst .terra/config
+
+time tar --zstd -cf /arch/.terra.tar.zst /root/.terra
 time tar --zstd -xf .terra.config.tar.zst
 
 curl http://localhost:1317/blocks/latest | jq .block.header  
+
+
+#
+mkdir /data/.terra
+terrad init --home=/data/.terra classic-01
+wget -O /data/.terra/config/genesis.json https://columbus-genesis.s3.ap-northeast-1.amazonaws.com/columbus-5-genesis.json
+cp /addrbook.json /data/.terra/config/addrbook.json
+terrad start --home=/data/.terra --x-crisis-skip-assert-invariants
